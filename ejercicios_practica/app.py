e01b3d6fe09405ed21a65e858b8f6ec31bb1bb35
Damisaf <109141323+Damisaf@users.noreply.github.com>
2022-10-20 19:47:28 -0300

    Add files via upload

--- ejercicios_practica/app.py
+++ ejercicios_practica/app.py
@@ -63,6 +63,13 @@ def personas():
         limit = 0
         offset = 0
 
+        limit_str = str(request.args.get('limit'))
+        offset_str = str(request.args.get('offset'))
+        if(limit_str is not None) and (limit_str.isdigit()):
+            limit = int(limit_str)
+        if(offset_str is not None) and (offset_str.isdigit()):
+            offset = int(offset_str)
+
         result = persona.report(limit=limit, offset=offset)
         return jsonify(result)
     except:
@@ -80,11 +87,17 @@ def registro():
             # Obtener del HTTP POST JSON el nombre y los pulsos
             # name = ...
             # age = ...
-
+            name = str(request.form.get('name')).lower()
+            age  = str(request.form.get('age'))
+            if(name is None or age.isdigit() is False):
+                # Datos ingresados incorrectos
+                return Response(status=400)
+            
             # Alumno: descomentar la linea persona.insert una vez implementado
             # lo anterior:
-            # persona.insert(name, int(age))
+            persona.insert(name, int(age))
             return Response(status=200)
+
         except:
             return jsonify({'trace': traceback.format_exc()})
 
